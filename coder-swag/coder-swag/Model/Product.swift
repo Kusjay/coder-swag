9a16d9829b323a54b00648718f7bc38ee863b86f
Kusjay <kelechiuma@gmail.com>
2018-11-29 13:42:28 +0100

    creating the data models

---
+++ coder-swag/coder-swag/Model/Product.swift
@@ -0,0 +1,21 @@
+//
+//  Product.swift
+//  coder-swag
+//
+//  Created by Kusjay on 11/29/18.
+//  Copyright © 2018 Kusjay. All rights reserved.
+//
+
+import Foundation
+
+struct Product {
+    private(set) public var title: String
+    private(set) public var price: String
+    private(set) public var imageName: String
+    
+    init(title: String, price: String, imageName: String) {
+        self.title = title
+        self.price = price
+        self.imageName = imageName
+    }
+}
